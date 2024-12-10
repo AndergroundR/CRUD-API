028c26d84af419979077d0304b8a8723d085b9ed
AndergroundR <anderson.vilhena0@gmail.com>
2024-12-10 10:03:20 -0300

    Adicionando método de listar itens e populando a base inicial.

--- crud.py
+++ crud.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+
 
 app = Flask(__name__)
 
@@ -8,8 +9,8 @@ items = []
 
 # Definindo o modelo de dados para os itens
 class ItemModel:
-    def __init__(self, item.id, name, value, creation_date, isElectronic):
-        self.id = item.id
+    def __init__(self, id, name, value, creation_date, isElectronic):
+        self.id = id
         self.name = name
         self.value = value
         self.creation_date = creation_date
@@ -24,13 +25,25 @@ class ItemModel:
             "isElectronic": self.isElectronic
         }
 
+# Populando a lista de itens com alguns dados iniciais
+items.append(ItemModel(1, "Item 1", 50, "2024-12-10 08:00:00", True))
+items.append(ItemModel(2, "Item 2", 56, "2024-12-11 09:00:00", False))
+items.append(ItemModel(3, "Item 3", 69, "2024-12-12 10:00:00", True))
 
 # Rota inicial
 @app.route('/')
 def index():
     return "CRUD API"
 
+# 1. Listar itens
+@app.get("/items")
+def list_items():
+    # Verifica se a lista está vazia
+    if not items:
+        # Retorna uma mensagem e um status 404 se não houver itens
+        return jsonify({"message": "Nenhum item encontrado"}), 404
 
+    return jsonify([item.json() for item in items]), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
